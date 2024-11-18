fb2727d5d925fa9abf778ee66911f4bce0b1260b
jdmolero <jdmolerogarciam@gmail.com>
2024-11-18 18:46:25 +0100

    Correción errores dockerización backend y postgres. Cambio a v17 de postgres.

--- src/backend/config/config.js
+++ src/backend/config/config.js
@@ -3,9 +3,9 @@ const process = require('process');
 
 module.exports = {
     "development": {
-        "username": process.env.DB_USERNAME,
-        "password": process.env.DB_PASSWORD,
-        "database": process.env.DB_DATABASE,
+        "username": process.env.POSTGRES_USER,
+        "password": process.env.POSTGRES_PASSWORD,
+        "database": process.env.POSTGRES_DB,
         "host": process.env.DB_HOST,
         "dialect": "postgres"
     },
